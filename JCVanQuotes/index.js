473e870b0aefedbe64e36911e6127f29cd81b412
JérémyDec <150056618+JeremyDecreton@users.noreply.github.com>
2024-01-30 16:44:28 +0100

    fin de journée

---
+++ JCVanQuotes/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jcvanquotes'
+});
+
+// Middleware pour autoriser les requêtes CORS
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Display Wisdom and Kicks like no Belgian before' });
+});
+
+app.get('/quote', (request, response) => {
+    pool.query('SELECT quote FROM quotes ORDER BY RAND() LIMIT 1', (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la récupération de la citation:', error);
+            response.status(500).json({ error: 'Erreur lors de la récupération de la citation.' });
+            return;
+        }
+        if (results.length === 0) {
+            response.status(404).json({ error: 'Aucune citation trouvée.' });
+            return;
+        }
+        response.json(results[0]);
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`);
+});
+
+
